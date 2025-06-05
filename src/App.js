eb927f8f63f1a401070bbdaff363c5333712a874
Banyeh Akika <banyehakika81@gmail.com>
2025-06-05 11:17:17 +0200

    A Member Directory

--- src/App.js
+++ src/App.js
@@ -1,38 +1,40 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Register from './components/pages/Register';
-import Login from './components/pages/Login';
-import UserList from './components/pages/UserList';
-import UserDetails from './components/pages/UserDetails';
-import DefaultPage from './components/pages/DefaultPage';
-import './App.css';
-function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem('user'));
+import React, {useState, useEffect} from "react";
 
+function Questionnaire(){
+  const [questionnaire, setQuestionnaire] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(`https://test.almamaters.club:9090/questionnaire/15`);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data)
+        setQuestionnaire(data.data);
+      } else{
+        console.log("Error fetching Data:", response.status);
+      }
+    };
+    fetchData();
+  }, []);
+
+  if (!questionnaire) {
+    return <div>Loading questionnaire...</div>;
+  }
   return (
-    <Router>
-      <DefaultPage isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}>
-        
-        <Routes>
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
+    <div>
+      <h1>{questionnaire.name}</h1>
+      <p>{questionnaire.description}</p>
 
-          {isLoggedIn ? (
-            <>
-              <Route path="/users" element={<UserList />} />
-              <Route path="/users/:id" element={<UserDetails />} />
-              <Route path="/" element={<Navigate to="/users" />} />
-            </>
-          ) : (
-            <>
-              <Route path="*" element={<Navigate to="/login" />} />
-              <Route path="/" element={<Navigate to="/login" />} />
-            </>
-          )}
-        </Routes>
-      </DefaultPage>
-    </Router>
-  );
+      {/* {questionnaire.questions.map((question) => (
+        <div key={question.id}>
+
+          </div>
+      ))} */}
+      
+      
+    </div>
+  )
 }
 
-export default App;
+export default Questionnaire;
+
